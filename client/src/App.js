b4004d0458106e16cbd7854475b0efeac22fae18
Pashovski <pashdoctor@gmail.com>
2022-04-11 16:42:32 -0400

    styled components intro

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
 import Landing from './pages/Landing'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: red;
+  color: white;
+  font-size: 1rem;
+`
+const ButtonSecond = styled.button`
+  background: blue;
+  color: white;
+  font-size: 1rem;
+`
 
 function App() {
   return (
     <div>
+      <Button>Click ME</Button>
+      <ButtonSecond>Click ME</ButtonSecond>
       <h1>Jobify</h1>
       <Landing />
     </div>
