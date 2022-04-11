d45196c6a2b24fc0f4899de8aefcddcd60ae9950
Pashovski <pashdoctor@gmail.com>
2022-04-11 16:23:36 -0400

    landing page structure

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,5 +1,28 @@
-export const Landing = () => {
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+const Landing = () => {
   return (
-    <div>Landing</div>
+    <main>
+      <nav>
+        <img src={logo} alt='jobify' className='logo' />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby cornhole health goth pabst, wolf kale chips thundercats
+            fixie. Banjo artisan craft beer single-origin coffee trust fund,
+            chambray kogi kickstarter affogato raw denim. Drinking vinegar
+            cliche sartorial, kinfolk offal tumeric leggings chillwave
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </main>
   )
 }
+export default Landing
