25daf1869a03dee8c291dfa61eb5f4c0212027a3
Pashovski <pashdoctor@gmail.com>
2022-04-17 21:26:28 -0400

    latest commit

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,17 @@
+import { useState, useEffect } from "react"
+import {Logo} from '../components'
+import Wrapper from "../assets/wrappers/Testing"
+
+
+const initialState = {
+  name:'',
+  email:'',
+  password:'',
+  isMember: true,
+}
+
 const Register = () => {
+  const [values, setValues] = useState(initialState)
   return <h1>Register</h1>
 }
 export default Register
