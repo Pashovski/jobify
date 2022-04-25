8e7799f491989ee6af0b74dfa7c90586d3ed1e61
Pashovski <pashdoctor@gmail.com>
2022-04-24 21:26:58 -0400

    fix CSS import

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Logo, FormRow, Alert } from '../components'
-import Wrapper from '../assets/wrappers/Testing'
+import Wrapper from '../assets/wrappers/RegisterPage'
 import { useAppContext } from '../context/appContext'
 
 const initialState = {
